53e9a1fbec066a9656100e66b7e198f4be0b6cc1
agarwal-kavya <110079729+agarwal-kavya@users.noreply.github.com>
2022-08-07 13:36:56 +0530

    Update iiitb_rc_tb.v

--- iiitb_rc_tb.v
+++ iiitb_rc_tb.v
@@ -1,26 +1,25 @@
+`timescale 1ns/1ps
 module iiitb_rc_tb;
-    reg clk;
-    reg reset;
-    wire [3:0] out;
-    reg [3:0] init;
-
-    iiitb_rc counter(clk, reset, init, out);
-
-    always #10 clk = ~clk;
-    initial
-        begin
-		$dumpfile("iiitb_rc.vcd");
+	reg clk;
+	reg ori;
+	wire[3:0] count;
+	iiitb_rc rc(clk, ori, count);
+	initial
+	begin
+		clk = 0;
+		ori = 0;
+	end
+	always
+	begin
+		#10;
+		clk = ~clk;
+	end
+	initial
+	begin
+		$dumpfile("iiitb_rc_out.vcd");
 		$dumpvars(0, iiitb_rc_tb);
-            $monitor($time," %b", out);
-            init[3] = 0;
-            init[2] = 0;
-            init[1] = 1;
-            init[0] = 0;
-            reset <= 1;  
-            clk <= 0; 
-            repeat (1) @ (posedge clk);  
-            reset <= 0;  
-            repeat (17) @ (posedge clk);  
-            $finish;
-        end
+		ori = 1;
+		#50;
+		ori = 0;
+	end
 endmodule
